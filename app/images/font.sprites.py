cf505ada6962a24960f1d3da975a85df7334d84f
Glenn Hill <glenn.m.hill@gmail.com>
2014-09-17 21:39:25 +1000

    Font.

---
+++ app/images/font.sprites.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+image('font.xcf')
+scale(10)
+
+sprites(glyphs=font(ur'''
+    01234567
+    89ABCDE
+    FGHIJKL
+    MNOPQRS
+    TUVWXYZ.
+    ''',
+    baselines=guides()['horizontal']
+))
